a4afc8e0243a7995e9d2aafa90e6953fdc2524ae
Enachescu Robert <enachescurobert@gmail.com>
2019-01-13 19:46:09 +0200

    Changed the mega.ino file

--- mega/mega.ino
+++ mega/mega.ino
@@ -1,28 +1,14 @@
 #include "LiquidCrystal.h"
-// initialize the library by providing the nuber of pins to it
 const int rs = 31, en = 30, d4 = 29, d5 = 28, d6 = 27, d7 = 26;
+// for Arduino Uno: rs = 13 , en = 12 , d4 = 11 , d5 = 10 , d6 = 9 , d7 = 8 ; 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 #include "SIM900.h"
 #include <SoftwareSerial.h>
-//If not used, is better to exclude the HTTP library,
-//for RAM saving.
-//If your sketch reboots itself proprably you have finished,
-//your memory available.
-//#include "inetGSM.h"
-
-//If you want to use the Arduino functions to manage SMS, uncomment the lines below.
 #include "sms.h"
 SMSGSM sms;
 
-//To change pins for Software Serial, use the two lines in GSM.cpp.
-
-//GSM Shield for Arduino
-//www.open-electronics.org
-//this code is based on the example of Arduino Labs.
-
-//Simple sketch to send and receive SMS.
-
+//gsm module
 int numdata;
 boolean started=false;
 char smsbuffer[160];
@@ -30,31 +16,29 @@ char n[20];
 
 //Ultrasonic
 
-const int trigPin = 40;
-const int echoPin = 41;
+const int trigPin = 40; //Arduino Uno : Pin 4
+const int echoPin = 41; //Arduino Uno : Pin 5
 long duration;
 int distance;
 
 //Buzzer
-int buzzer = 36;//the pin of the active buzzer
+int buzzer = 36;//pin 4 for Arduino
 
 
 void setup() 
 {
    lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Totul OK!");
+   lcd.print("Totul OK!");
 
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
   //Buzzer
    pinMode(buzzer,OUTPUT);//initialize the buzzer pin as an output
 
-
   //Serial connection.
-  Serial.begin(9600);
-  Serial.println("GSM Shield testing.");
+   Serial.begin(9600);
+   Serial.println("GSM Shield testing.");
   //Start configuration of shield with baudrate.
   //For http uses is raccomanded to use 4800 or slower.
   if (gsm.begin(2400)){
@@ -66,13 +50,6 @@ void setup()
 
 /*
   if(started){
-    //Enable this two lines if you want to send an SMS.
-    if (sms.SendSMS("0736094704", "Arduino SMS"))
-      Serial.println("\nSMS sent OK");
-  }
-
-
-  if(started){
     //Read if there are messages on SIM card and print them.
     if(gsm.readSMS(smsbuffer, 160, n, 20))
     {
@@ -80,7 +57,6 @@ void setup()
       Serial.println(smsbuffer);
     }
     delay(1000);
-  
 }
 */
 }
@@ -101,31 +77,53 @@ void loop()
     // Prints the distance on the Serial Monitor
     Serial.print("Distance: ");
     Serial.println(distance);
+    
     if(distance>50){
-    //Enable this two lines if you want to send an SMS.
     if (sms.SendSMS("0736094704", "VAGxy"))
       Serial.println("\nSMS sent OK");
+      sms.SendSMS("0754490582", "VAGxy");
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("VAGxy");
 
-       //output an frequency
-   for(i=0;i<80;i++)
-   {
-    digitalWrite(buzzer,HIGH);
-    delay(1000);//wait for 1ms
-    digitalWrite(buzzer,LOW);
-    delay(1);//wait for 1ms
-    }
-    //output another frequency
-     for(i=0;i<100;i++)
-      {
+     //Buzzer
+       for(i=0;i<80;i++)
+       {
         digitalWrite(buzzer,HIGH);
-        delay(2);//wait for 2ms
+        delay(200);//wait for 1ms
         digitalWrite(buzzer,LOW);
-        delay(2);//wait for 2ms
-      }
+        delay(20);//wait for 1ms
+        }
+
 }
+
+    if(started){
+        //Serial.println("AT+CMGL=\"ALL\"");
+    //Read if there are unread messages on SIM card and print them.
+    if(gsm.readSMS(smsbuffer, 160, n, 20))
+    {
+      String exemplu = smsbuffer;
+      if(exemplu.startsWith("VAG")){
+        
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(exemplu.substring(0,5));
+        
+        //Buzzer 
+         unsigned char i;
+         for(i=0;i<80;i++)
+         {
+          digitalWrite(buzzer,HIGH);
+          delay(200);
+          digitalWrite(buzzer,LOW);
+          delay(20);
+         }
+      }
+      }
+      Serial.println(n);
+      Serial.println(smsbuffer);
+     
+    }
 
   
     delay(5000);
